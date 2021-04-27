b6f2d9e29871c1c84e443a79e4e6094fd79211b0
Ram <77988163+ramatcoding@users.noreply.github.com>
2021-04-27 11:09:04 -0700

    Add files via upload

--- js/gameControl.js
+++ js/gameControl.js
@@ -1,6 +1,10 @@
 /*=================================================================================================
+	For notes and explanations on this script and its functions, see the version of the script
+	contained in the "Exercise17\complete\" folder
+=================================================================================================*/
+
+/*=================================================================================================
 -- GLOBAL VARIABLES
-	These variables will be available to all of the page's scripts
 -------------------------------------------------------------------------------------------------*/
 var gameEngine;
 /*===============================================================================================*/
@@ -12,126 +16,100 @@ window.addEventListener("load", initialiseGame, false);
 /*===============================================================================================*/
 
 /*=================================================================================================
--- FUNCTION: initialiseGame()
-	Starts the game engine, assigning it to the global variable called 'gameEngine'.
-	Also starts a new game.
+-- FUNCTION: initialiseGame():Void
 -------------------------------------------------------------------------------------------------*/
 function initialiseGame() {
-	gameEngine = new com.flametreepublishing.cfk.HungryMan();
-	startNewGame();
+	gameEngine = new com.flametreepublishing.cfk.CodeBreaker();
+	showGameSetupPanel();
 }
 /*===============================================================================================*/
 
 /*=================================================================================================
--- FUNCTION: startNewGame()
-	Starts a new game by clearing anything that may be left over from a previous game and then
-	resetting all game parameters
+-- FUNCTION: showGameSetupPanel():Void
 -------------------------------------------------------------------------------------------------*/
-function startNewGame() {
-	gameEngine.resetGameGraphics();
-	gameEngine.loadNewWord();
-	var wordLength = gameEngine.getLengthOfCurrentWord();
-	buildCurrentWordDisplay(wordLength);
-	buildLetterButtons();
+function showGameSetupPanel() {
+	document.getElementById("gameOverPanel").style.display = "none";
+	document.getElementById("setupNewGameButton").style.display = "none";
+	document.getElementById("gameSetupPanel").style.display = "block";
 }
 /*===============================================================================================*/
 
 /*=================================================================================================
--- FUNCTION: buildCurrentWordDisplay(numberOfLetters:Number):Void
-	A one row table is used for displaying the "slots" in which the letters of the mystery
-	word are displayed (and shown as empty when the letter is unguessed).
-	This method clears the tr element containing the current word/guesses and then creates a new
-	tr element with the number of td elements indicated by numberOfLetters 
+-- FUNCTION: startGame():Void
 -------------------------------------------------------------------------------------------------*/
-function buildCurrentWordDisplay(numberOfLetters) {
-	var currentWordTBody = document.getElementById("currentWordTable").getElementsByTagName("tbody")[0];
-	while (currentWordTBody.hasChildNodes()) {
-		currentWordTBody.removeChild(currentWordTBody.firstChild);
-	}
-	var newRow = document.createElement("tr");
-	var newCell;
-	for (var i = 0; i < numberOfLetters; i++) {
-		newCell = document.createElement("td");
-		newRow.appendChild(newCell);
-	}
-	currentWordTBody.appendChild(newRow);
+function startGame() {
+	document.getElementById("gameSetupPanel").style.display = "none";
+	document.getElementById("setupNewGameButton").style.display = "inline";
+	var numSymbols = document.getElementById("numSymbolsOption").value;
+	var codeLength = document.getElementById("codeLengthOption").value;
+	var maxGuesses = document.getElementById("maxGuessesOption").value;
+	gameEngine.startNewGame(numSymbols, codeLength, maxGuesses);
 }
 /*===============================================================================================*/
 
 /*=================================================================================================
--- FUNCTION: buildLetterButtons():Void
-	Clears the letter buttons from the previous game (if any exist) and then creates a new set
-	of 26 buttons that the player clicks to guess a letter. The buttons are arranged in two rows,
-	each row being a div element
+-- FUNCTION: cycleCodeButton(eventObj:Event):Void
 -------------------------------------------------------------------------------------------------*/
-function buildLetterButtons() {
-	var letterButtonsWrapper = document.getElementById("letterButtonsWrapper");
-	while (letterButtonsWrapper.hasChildNodes()) {
-		letterButtonsWrapper.removeChild(letterButtonsWrapper.firstChild);
-	}
-	var row1 = document.createElement("div");
-	var row2 = document.createElement("div");
-	for (var i = 65; i <= 90; i++) {
-		var newButton = document.createElement("button");
-		var theLetter = String.fromCharCode(i);
-		newButton.innerHTML = theLetter;
-		newButton.setAttribute("letter", theLetter);
-		newButton.className = "untried";
-		newButton.addEventListener("click", guessLetter, false);
-		if (i < 78) {
-			row1.appendChild(newButton);
+function cycleCodeButton(eventObj) {
+	if (gameEngine.getGameStatus() == "inProgress") {
+		var codeButtonElement = eventObj.currentTarget;
+		var cycleDirection;
+		if (eventObj.type == "click") {
+			cycleDirection = "forward"
 		} else {
-			row2.appendChild(newButton);
+			eventObj.preventDefault();
+			cycleDirection = "backward"
 		}
+		var buttonNum = Number(codeButtonElement.getAttribute("codeButtonNum"));
+		var currentSymbolId = codeButtonElement.getAttribute("symbolId");
+		gameEngine.cycleInputCodeButton(buttonNum, currentSymbolId, cycleDirection);
 	}
-	letterButtonsWrapper.appendChild(row1);
-	letterButtonsWrapper.appendChild(row2);	
+	return(false);
 }
 /*===============================================================================================*/
-
 
 /*=================================================================================================
--- FUNCTION / EVENT HANDLER: tryLetter(eventObj:Object)
-	Triggered when the player clicks on a letter button. Handles the subsequent actions
+-- FUNCTION: submitInputCode(eventObj:Event):Void
 -------------------------------------------------------------------------------------------------*/
-function guessLetter(eventObj) {
-	if (gameEngine.isGameOver()) {
-		//the game is over, so exit the function without doing anything
-		return;
-	}
-	var clickedButton = eventObj.target;
-	var letterToGuess = clickedButton.getAttribute("letter");
-	if (gameEngine.hasLetterBeenTried(letterToGuess)) {
-		window.alert("You have already tried the letter " + letterToGuess);
-		return;
-	}
-	var guessResult = gameEngine.guessLetter(letterToGuess);
-	var isGameOver;
-	var isGameLost = false;
-	if (guessResult.length == 0) {
-		clickedButton.className = "nomatch";
-		isGameOver = gameEngine.registerIncorrectGuess();
-		if(isGameOver) {
-			isGameLost = true;
+function submitInputCode(eventObj) {
+	if (gameEngine.getGameStatus() == "inProgress") {
+		var inputCodeButtons = document.getElementsByClassName("inputCodeButton");
+		var inputCode = new Array();
+		for (var i = 0; i < inputCodeButtons.length; i++) {
+			inputCode.push(inputCodeButtons[i].getAttribute("symbolId"));
 		}
-	} else {
-		clickedButton.className = "match";
-		var currentWordCells = document.getElementById("currentWordWrapper").getElementsByTagName("td");
-		for (var i = 0; i < guessResult.length; i++) {
-			var thisMatchPosition = guessResult[i];
-			currentWordCells[(thisMatchPosition)].innerHTML = letterToGuess;
-		}
-		isGameOver = gameEngine.isWordGuessed();
-	}
-	if (isGameOver) {
-		if (isGameLost) {
-			gameEngine.eatTheFood();
-		} else {
-			gameEngine.reportGameOver();
+		var codeResult = gameEngine.tryCode(inputCode);
+		gameEngine.displayResult(inputCode, codeResult);
+		var gameStatus = gameEngine.getGameStatus();
+		if (gameStatus == "won" || gameStatus == "lost") {
+			reportGameOver(gameStatus);
 		}
 	}
 }
 /*===============================================================================================*/
+
+/*=================================================================================================
+-- FUNCTION: reportGameOver(gameStatus:String):Void
+-------------------------------------------------------------------------------------------------*/
+function reportGameOver(gameStatus) {
+		var gameOverPanel = document.getElementById("gameOverPanel");
+		var messageElement = document.getElementById("gameOverMeassge");
+		var numberOfPossibleCodes = gameEngine.getNumberOfPossibleCodes();
+		var currentGuessCount = gameEngine.getCurrentGuessCount();
+
+		if (gameStatus == "won") {
+			var messageText = "Success!<br><br>There were " + numberOfPossibleCodes.toLocaleString() + 
+			" possible combinations for the hidden code, and you cracked it in " + currentGuessCount + " tries."
+		} else {
+			var messageText = "You failed to crack the code!<br><br>There were " + numberOfPossibleCodes +
+			" possible combinations for the hidden code.";
+		}
+		messageElement.innerHTML = messageText;
+		gameOverPanel.style.display = "block";
+}
+
+
+
 
 
 
